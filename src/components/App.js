4280dc87b34c29a92e87cab66a4243fe35794ac4
resha <reshavkumar9837@gmail.com>
2023-01-15 13:58:16 +0530

    add

---
+++ src/components/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "../styles/App.css";
+const App = () => {
+  return (
+    <div id="main">
+      <div class="container">
+                <div class="community">
+                  <h2 class="community-header">Join our community</h2>
+                </div>
+                <div class="card-details">
+                  <div class="card">
+                    <p class="card-header">Monthly Subscription</p>
+                  </div>
+                  <div class="why-us">
+                    <h4 class="why-us-header">Why Us</h4>
+                  </div>
+                </div>
+              </div>
+    </div>
+  );
+};
+
+export default App;
